05748b454125889866232dc1c647a4b56c3396ea
sakibmahmodul <sakibuddessho89@gmail.com>
2025-04-30 16:53:54 +0600

    Install next

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+      <main className="">
+        <h1>Let's Build An AI Productivity App</h1>
+        <Button variant="ghost">Click Me</Button>
+      </main>
+  );
+}
